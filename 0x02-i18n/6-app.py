0350480c9cf4ca601ba5223aa6a4bebb1aa537f4
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-07-30 14:48:27 +0000

    new commit

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
-from flask import Flask, render_template, request, g
-from flask_babel import Babel, _
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
+from typing import (
+    Dict,
+    Union
+)
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -11,25 +37,13 @@ users = {
 }
 
 
-class Config:
-    """Configuration for Babel"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel()
-
-
-def get_user():
+def get_user() -> Union[Dict, None]:
     """
     Returns a user dictionary or None if ID value can't be found
     or if 'login_as' URL parameter was not found
     """
     id = request.args.get('login_as', None)
-    if id is not None and int(id) in users.keys():
+    if id and int(id) in users.keys():
         return users.get(int(id))
     return None
 
@@ -43,8 +57,11 @@ def before_request():
     g.user = user
 
 
+@babel.localeselector
 def get_locale():
-    """Select and return best language match based on supported languages"""
+    """
+    Select and return best language match based on supported languages
+    """
     loc = request.args.get('locale')
     if loc in app.config['LANGUAGES']:
         return loc
@@ -58,14 +75,13 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
-
-
 @app.route('/', strict_slashes=False)
-def home() -> str:
-    """Basic Flask app"""
-    return render_template('6-index.html')
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
